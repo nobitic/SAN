0133912502dc0612bb1903a382462f5434b73e16
nobitic <100597038+nobitic@users.noreply.github.com>
2022-03-03 15:57:13 +0000

    Update __init__.py

--- LuciferMoringstar_Robot/__init__.py
+++ LuciferMoringstar_Robot/__init__.py
@@ -16,26 +16,18 @@ Basic Commads
 /start :- check am Alive
 
 Bot Owner Only
-/broadcast Replay Any Message or Media
 /stats user Status
-/ban_user  click ban_user more info
-/unban_user click unban_user more info
-/banned_users Banned User Details
-/total How Many Files Added In Database
-/logger  Get Logs
-/delete Delete File From Database
 
 """
 
 ABOUT = """
-➥ Name :  Auto Filter V3
+➥ Name :  Auto Filter
     
-➥ Creator : [Naughty PROFESSOR](
-https://youtube.com/c/NaughtyPROFESSOR)
+➥ Creator : [LUCIFER]
 
 ➥ Language : Python3
 
 ➥ Library : Pyrogram Asyncio 1.13.0
 
-➥ Tutorial : [Click Me](https://youtu.be/5hnYOKBzyi8)
+➥ Tutorial : (https://t.me/MF2move)
 """
