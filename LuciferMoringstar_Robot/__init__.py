2feb51d66de3630b90fcf438c08c91fbad953bd3
nobitic <100597038+nobitic@users.noreply.github.com>
2022-03-04 08:41:56 +0000

    Update __init__.py

--- LuciferMoringstar_Robot/__init__.py
+++ LuciferMoringstar_Robot/__init__.py
@@ -16,12 +16,20 @@ Basic Commads
 /start :- check am Alive
 
 Bot Owner Only
+/broadcast Replay Any Message or Media
 /stats user Status
+/ban_user  click ban_user more info
+/unban_user click unban_user more info
+/banned_users Banned User Details
+/total How Many Files Added In Database
+/logger  Get Logs
+/delete Delete File From Database
+/index  add file
 
 """
 
 ABOUT = """
-➥ Name :  MF MOVE GROUP
+➥ Name :  FM_MOVE_CHAT
     
 ➥ Creator : [LUCIFER]
 
@@ -29,5 +37,5 @@ ABOUT = """
 
 ➥ Library : Pyrogram Asyncio 1.13.0
 
-➥ Tutorial : (https://t.me/MF_Movies)
+➥ Tutorial : (https://t.me/MF2move)
 """
